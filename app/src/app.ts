d7c34b6c30022d442769faafe035b436dd876ecd
Matheus Pino <matheus.pino@hotmail.com>
2023-08-04 12:25:28 -0300

    Primeiro commit

---
+++ app/src/app.ts
@@ -0,0 +1,21 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
+}
+
+const importaDados = document.querySelector("#botao-importa")
+if (importaDados){
+    importaDados.addEventListener("click", () => {
+        controller.importaDados();
+    } )
+} else {
+    throw Error("Não foi possivel localizar os dados.")
+}
